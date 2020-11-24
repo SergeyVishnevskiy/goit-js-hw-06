1a7100a96d0be0213f9cd7df67d5f4c961f31fa0
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-24 21:41:52 +0200

    task-03 done

--- JS/task-03.js
+++ JS/task-03.js
@@ -1,8 +1,11 @@
 // Задание 3
 // Получить массив имен пользователей по полу (поле gender).
+import users from "./users.js";
 
 const getUsersWithGender = (users, gender) => {
-  // твой код
+  return users
+    .filter((user) => user.gender === gender)
+    .map((user) => user.name);
 };
 
 console.log(getUsersWithGender(users, "male")); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
