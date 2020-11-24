44f461dacff54d270e6d2e998a2ce8fa5b035a4a
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-24 21:41:45 +0200

    task-02 - done

--- JS/task-02.js
+++ JS/task-02.js
@@ -3,7 +3,6 @@
 import users from "./users.js";
 
 const getUsersWithEyeColor = (users, color) => {
-  // твой код
+  return users.filter((user) => user.eyeColor === color);
 };
-
 console.log(getUsersWithEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
