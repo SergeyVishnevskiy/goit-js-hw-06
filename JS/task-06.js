cfd0d5caa0b9baf437513c10fbda4ff182bdf441
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-25 07:45:26 +0200

    task-06 done

--- JS/task-06.js
+++ JS/task-06.js
@@ -3,7 +3,7 @@
 import users from "./users.js";
 
 const getUsersWithAge = (users, min, max) => {
-  return users.filter;
+  return users.filter((user) => user.age > min && user.age < max);
 };
 
 console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
