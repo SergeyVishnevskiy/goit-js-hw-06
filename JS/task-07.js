85cafe42705004845be0bea659c5e900bed4b0a5
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-25 08:04:23 +0200

    task-07 done

--- JS/task-07.js
+++ JS/task-07.js
@@ -3,7 +3,10 @@
 import users from "./users.js";
 
 const calculateTotalBalance = (users) => {
-  // твой код
+  return users.reduce(
+    (totalBalance, user) => (totalBalance += user.balance),
+    0
+  );
 };
 
 console.log(calculateTotalBalance(users)); // 20916
