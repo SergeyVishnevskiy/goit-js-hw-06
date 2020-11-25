4c436dae41ae438441239d5d5a321743262b3619
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-25 15:46:21 +0200

    task-09 done

--- JS/task-09.js
+++ JS/task-09.js
@@ -3,7 +3,9 @@
 import users from "./users.js";
 
 const getNamesSortedByFriendsCount = (users) => {
-  // твой код
+  return users
+    .sort((user1, user2) => user1.friends.leygth - user2.friends.length)
+    .map((user) => user.name);
 };
 
 console.log(getNamesSortedByFriendsCount(users));
